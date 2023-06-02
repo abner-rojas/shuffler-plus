0d58ca46b4ce596f1fda0e7b2733426851fca054
Abner Rojas <abnerrojas@192.168.11.8>
2023-06-02 09:29:36 -0400

    updated json team name

--- src/App.tsx
+++ src/App.tsx
@@ -40,7 +40,7 @@ function App() {
   const [questions, setQuestions] = useState<Array<Question>>([])
   const [quotes, setQuotes] = useState<Array<Quote>>([])
   const [fortunes, setFortunes] = useState<Array<string>>([])
-  const [squadName, setSquadName] = useState<string>('Squad B')
+  const [squadName, setSquadName] = useState<string>('Bumblebee Tuna')
 
   useEffect(() => {
     setTimeout(() => setLoading(false), 4000)
